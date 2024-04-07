2bc83e7d32d0acfd3f7579bed0ffd578affed223
whoisjose569 <jose.vitor.araujo569@hotmail.com>
2024-04-06 22:43:13 -0300

    feat: Starting implementing UserFinder

--- src/data/use_cases/user_finder_test.py
+++ src/data/use_cases/user_finder_test.py
@@ -1,7 +1,27 @@
-from src.infra.db.repositories.users_repository import UsersRepository
+from src.infra.db.tests.users_repository import UsersRepositorySpy
 from .user_finder import UserFinder
 
 
 def test_find():
-    repo = UsersRepository
+    first_name = 'meunome'
+    repo = UsersRepositorySpy()
     user_finder = UserFinder(repo)
+
+    response = user_finder.find(first_name)
+
+    assert repo.select_user_attributes["first_name"] == first_name
+
+    assert response["type"] == "Users"
+    assert response["count"] == len(response["attributes"])
+    assert response["attributes"] != []
+
+def test_find_error_in_vlaid_name():
+
+    first_name = 'meunome123'
+    repo = UsersRepositorySpy()
+    user_finder = UserFinder(repo)
+
+    try:
+        response = user_finder.find(first_name)
+    except Exception as expection:
+        assert str(expection) == "Nome invalido para a busca"
